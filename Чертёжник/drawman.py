52fbe586699b2c89ed78f513be9a01fa6e00bf49
Mihai <m2bani@gmail.com>
2016-07-26 17:38:14 +0300

    Создан чертежник на основе ГРИС Черепаха

--- Чертёжник/drawman.py
+++ Чертёжник/drawman.py
@@ -2,13 +2,12 @@ from turtle import Turtle
 
 
 def init_drawman():
-    global t, x_current, y_current, drawman_scale
+    global t, x_current, y_current
     t = Turtle()
     t.penup()
     x_current = 0
     y_current = 0
     t.goto(x_current, y_current)
-    drawman_scale = 10
 
 def test_drawman():
     """
@@ -32,14 +31,17 @@ def pen_up():
 
 
 def on_vector(dx, dy):
-    to_point(x_current + dx, y_current + dy)
+    global x_current, y_current
+    x_current += dx
+    y_current += dy
+    t.goto(x_current, y_current)
 
 
 def to_point(x, y):
     global x_current, y_current
     x_current = x
     y_current = y
-    t.goto(drawman_scale*x_current, drawman_scale*y_current)
+    t.goto(x_current, y_current)
 
 
 init_drawman()
